3740791e8cd78cf3af1834410caaee80a70d4fc8
Tomoyuki Nishimura <tomoyuki.nishimura@w.ntt.com>
2021-05-31 12:24:04 +0900

    update gitignore

--- CreateCloudConnection/index.ts
+++ CreateCloudConnection/index.ts
@@ -1,17 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-// import { getInformation as getJuniperInformation, setSOConfiguration as setJuniperSOConfiguration } from "./juniper"
+import { getInformation as getJuniperInformation, setSOConfiguration as setJuniperSOConfiguration } from "./juniper"
 import { getInformation as getAristaInformation, setSOConfiguration as setAristaSOConfiguration } from "./arista"
 
 const createCloudConnection: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     console.log('CreateCloudConnection function processed a request.');
     try {
-        // await getJuniperInformation();
-        // await setJuniperSOConfiguration();
+        await getJuniperInformation();
+        await setJuniperSOConfiguration();
         await getAristaInformation();
         await setAristaSOConfiguration();
-        context.res = {
-            status: 200
-        };
+        context.res = {status: 200};
     } catch (error) {
         context.res = {
             status: 500,
